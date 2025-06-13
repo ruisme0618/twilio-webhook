2f35f442a22ad161481ac80a8ef3bc161984cb7c
ruisme0618 <rui.smets@gmail.com>
2025-06-13 19:47:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const twilio = require('twilio');
 const app = express();
 
-console.log('Webhook server is alive');
-
-app.use(express.urlencoded({ extended: true })); // Voor x-www-form-urlencoded data van Twilio
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/webhook', (req, res) => {
   console.log('✅ Ontvangen van Twilio:', req.body);
@@ -18,11 +15,7 @@ app.post('/webhook', (req, res) => {
     reply = "We zijn dagelijks open van 12:00 tot 22:00.";
   }
 
-  // ➤ Verstuur TwiML-response naar Twilio
-  const twiml = new twilio.twiml.MessagingResponse();
-  twiml.message(reply);
-  res.type('text/xml');
-  res.send(twiml.toString());
+  res.json({ reply });
 });
 
 const port = process.env.PORT || 3000;
